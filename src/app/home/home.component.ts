e594b0e99044be26f24d932c55c9c6095f742a02
AndresPortoCastro <101732746+leviescan@users.noreply.github.com>
2024-02-01 12:51:58 -0500

    actualization

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Producto } from '../producto.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+
+  productosList: Producto[]= [];
+
+
+
+  constructor(private elementRef: ElementRef) { }
+
+
+
+  ngOnInit() {
+    this.cargarEventListeners();
+    
+  }
+
+  
+
+
+   cargarEventListeners() {
+    
+}
+
+comprarElemento(e:any) {
+  
+}
+
+leerDatosElemento(elemento: ElementRef): void {
+  const infoElemento = {
+    imagen: elemento.nativeElement.querySelector('img').src,
+    titulo: elemento.nativeElement.querySelector('h3').textContent,
+    precio: elemento.nativeElement.querySelector('.Precio').textContent,
+    id: elemento.nativeElement.querySelector('.agregar-carrito').getAttribute('data-id')
+  };
+console.log(infoElemento)
+}
+
+ insertarCarrito(elemento:any) {
+   
+}
+
+eliminarElemento(e:any) {
+ 
+}
+
+ vaciarCarrito() {
+  
+}
 
 }
---
+++ src/app/producto.model.ts
@@ -0,0 +1,6 @@
+export interface Producto {
+    id:any;
+    imagen:any;
+    titulo:any;
+    precio:any;
+}
