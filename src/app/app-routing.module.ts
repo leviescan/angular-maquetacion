c4a7460327aa0e8434b1ab9301ca8262eb0fbda5
AndresPortoCastro <101732746+leviescan@users.noreply.github.com>
2024-01-31 19:34:41 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarritoComponent } from './carrito/carrito.component';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+{ path: 'car', component: CarritoComponent },
+{ path: 'productos', component: ProductosComponent },
+{ path: 'contacto', component: ContactosComponent },
+{ path: 'servicios', component: ServiciosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarritoComponent,
+    HomeComponent,
+    ProductosComponent,
+    ContactosComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
